877c2518a0280c38568f0b0c219741b011c6db83
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-03-10 09:54:52 +0100

    evaluating display added

--- E1/src/Main.java
+++ E1/src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         String folderPath = "F:\\sztuczna_inteligencja\\flo_dane_v1.2";
-        Factory factory = new Factory(FactoryValues.HARD, folderPath);
+        Factory factory = new Factory(FactoryValues.EASY, folderPath);
         System.out.println(factory);
 
         var initPopulation = factory.createInitMachinesPositions();
         factory.displayPopulation(initPopulation);
+        var eval = factory.evaluatePopulation(initPopulation);
+        System.out.printf("\nEvaluated: %s", eval);
     }
 }
