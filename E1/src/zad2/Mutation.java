4dfaf01bc6438b08bb526f0ef9168fbfde772d02
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-03-23 11:22:41 +0100

    mutation changed from point to multiple points

--- E1/src/zad2/Mutation.java
+++ E1/src/zad2/Mutation.java
@@ -20,22 +20,18 @@ public class Mutation {
         return child;
     }
 
-//    public static int[] mutate(int[] child, double percentage) {
-//        int[] newChild = child.clone();
-//        int lenToChange = (int) (newChild.length * percentage);
-//        int maxStartInx = newChild.length - lenToChange;
-//        int startInx = random.nextInt(maxStartInx);
-////        System.out.println(startInx);
-////        System.out.println(lenToChange);
-////        System.out.println(Arrays.toString(child));
-//
-//        int first = newChild[startInx];
-//        for (int i = 0; i < lenToChange - 1; i++) {
-//            newChild[startInx + i] = child[startInx + i + 1];
-//        }
-//        newChild[startInx + lenToChange - 1] = first;
-//        return newChild;
-//    }
+    public static int[] mutate(int[] child, int lenToChange) {
+        int maxStartInx = child.length - lenToChange;
+        int startInx = random.nextInt(maxStartInx);
+        for (int i = lenToChange - 1; i > 0; i--) {
+            int j = random.nextInt(i+1);
+            int temp = child[i + startInx];
+            child[i + startInx] = child[j + startInx];
+            child[j + startInx] = temp;
+        }
+
+        return child;
+    }
 }
 
 class MutationTest {
@@ -57,5 +53,27 @@ class MutationTest {
         }
         Assertions.assertEquals(0, sumChange);
         Assertions.assertEquals(2, sumChangeCopy);
+    }
+    @Test
+    public void mutationTest2() {
+        var child = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        var childCopy = child.clone();
+        var mutated = Mutation.mutate(child, 4);
+        int sumChange = 0;
+        int sumChangeCopy = 0;
+
+        for (int i = 0; i < child.length; i++) {
+            if(child[i] != mutated[i]) {
+                sumChange += 1;
+            }
+            if(childCopy[i] != mutated[i]) {
+                sumChangeCopy += 1;
+            }
+        }
+        System.out.println(Arrays.toString(childCopy));
+        System.out.println(Arrays.toString(child));
+        System.out.println(Arrays.toString(mutated));
+        Assertions.assertEquals(0, sumChange);
+        Assertions.assertTrue(sumChangeCopy <= 4);
     }
 }
