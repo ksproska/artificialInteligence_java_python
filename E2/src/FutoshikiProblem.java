e5520fc13f1abb7a367dc6a4fb69e1b3a493bdff
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-03-30 08:03:54 +0200

    E2 reading Futoshiki works

--- E2/src/FutoshikiProblem.java
+++ E2/src/FutoshikiProblem.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class FutoshikiProblem {
     public final FutoshikiEnum chosenProblem;
     public final int x, y;
+    public final int gridX, gridY;
     public final ArrayList<Object> problem;
 //    public final ArrayList<Integer> indexesToFill;
 
@@ -16,6 +17,8 @@ public class FutoshikiProblem {
         this.chosenProblem = chosenProblem;
         this.x = FutoshikiConsts.getSize(chosenProblem)[0];
         this.y = FutoshikiConsts.getSize(chosenProblem)[1];
+        gridX = this.x * 2 - 1;
+        gridY = this.y * 2 - 1;
         this.problem = new ArrayList<>();
 //        indexesToFill = new ArrayList<>();
         readProblem(ProjectConsts.folderPath);
@@ -26,6 +29,7 @@ public class FutoshikiProblem {
         String filename = String.format("%s\\%s_%sx%s", folderPath, "futoshiki", x, y);
         try {
             Scanner scanner = new Scanner(new File(filename));
+            int rowCounter = 0;
             while (scanner.hasNextLine()) {
                 var nextLine = scanner.nextLine().split("");
                 for (int i = 0; i < nextLine.length; i++) {
@@ -38,8 +42,12 @@ public class FutoshikiProblem {
                     if(nextValue == "x") {
                         nextValue = new Integer(null);
                     }
-                    problem.add(i, nextValue);
+                    problem.add(nextValue);
+                    if(rowCounter % 2 == 1 && i < x - 1) {
+                        problem.add(null);
+                    }
                 }
+                rowCounter += 1;
             }
             scanner.close();
         } catch (IOException e) {
@@ -47,6 +55,29 @@ public class FutoshikiProblem {
             System.out.println(errorMessage);
         }
     }
+
+    public String toDisplay(ArrayList<Object> grid) {
+        var allToDisplay = "";
+        for (int i = 0; i < grid.size(); i++) {
+            if (grid.get(i) == null
+                    || grid.get(i).equals("-")
+            ) {
+                allToDisplay += " ";
+            }
+            else {
+                allToDisplay += grid.get(i);
+            }
+            allToDisplay += "   ";
+            if((i + 1) % gridX == 0) {
+                allToDisplay += "\n";
+            }
+        }
+        allToDisplay = allToDisplay.substring(0, allToDisplay.length() - 3);
+        return allToDisplay;
+    }
+
+    @Override
+    public String toString() { return chosenProblem + "\n" + toDisplay(problem); }
 }
 
 class FutoshikiTests {
@@ -55,6 +86,6 @@ class FutoshikiTests {
     @Test
     void test1() {
         futoshikiProblem = new FutoshikiProblem(FutoshikiEnum.F4x4);
-        System.out.println(futoshikiProblem.problem);
+        System.out.println(futoshikiProblem);
     }
 }
