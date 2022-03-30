2859b5749d2ced8789a0ae7ed67680085cc6f664
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-03-30 23:44:52 +0200

    E2 housekeeping 2

--- E2/src/CSP_Variable.java
+++ E2/src/CSP_Variable.java
@@ -11,24 +11,16 @@ public class CSP_Variable<T> {
         wasVariableUsed = false;
     }
 
-    public void add(T variable) {
-        domain.add(variable);
+    public CSP_Variable(CSP_Variable<T> other) {
+        this.variableIndex = other.variableIndex;
+        this.domain = new ArrayList<>(other.domain);
+        wasVariableUsed = other.wasVariableUsed;
     }
 
-    public void remove(T variable) {
-        domain.remove(variable);
-    }
-
-    public void removeAll() {
-        domain = new ArrayList<>();
-    }
-
-    public ArrayList<T> getDomain() {
-        return domain;
-    }
+    public void add(T variable) { domain.add(variable); }
+    public void removeAll() { domain = new ArrayList<>(); }
+    public ArrayList<T> getDomain() { return domain; }
 
     @Override
-    public String toString() {
-        return variableIndex + ":" + domain;
-    }
+    public String toString() { return variableIndex + ":" + domain; }
 }
